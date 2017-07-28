026cddfa08f49652686ad3ecb8719cd0af88112b
Mraleksa <a.gribanovskiy@gmail.com>
2017-07-28 09:14:15 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -9,7 +9,7 @@ var p=0; var p2=0;
 
 //db.run("DELETE FROM data");
 
-var currentCount =  "2017-05-24T18:03:44.827165+03:00"
+var currentCount =  "2017-06-09T15:50:12.678303+03:00"
 
 //db.each("SELECT dateModified FROM data ORDER BY dateModified DESC LIMIT 1", function(err, timeStart) {
       
